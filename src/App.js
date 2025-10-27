d762c2f29cde78df1672f3b67a1bf386dac25df3
user <ssharp0614@gmail.com>
2025-10-27 21:04:49 +0900

    feat(App): add winner selection logic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import Cars from './Cars.js';
 
 class App {
+  constructor() {
+    this.winners = [];
+  }
+
+  selectWinner(cars) {
+    cars.sort();
+    const CARS_DATA = cars.cars;
+    for (let i = 0; i < CARS_DATA.length; i += 1) {
+      if (i === 0) {
+        this.winners.push(CARS_DATA[i].name);
+      } else if (CARS_DATA[i].steps === CARS_DATA[i - 1].steps) {
+        this.winners.push(CARS_DATA[i].name);
+      } else {
+        break;
+      }
+    }
+  }
+
   async run() {
     const CAR_NAMES_STR = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const TRY_COUNT = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
@@ -13,6 +31,8 @@ class App {
       CARS.carsStepsPrint();
       MissionUtils.Console.print('');
     }
+
+    this.selectWinner(CARS);
   }
 }
 
