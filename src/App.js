e58759b92762c4783233f8b091de832d84ab23f9
user <ssharp0614@gmail.com>
2025-10-27 20:48:31 +0900

    feat(App): integrate Cars class for handling car name inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import Cars from './Cars';
 
 class App {
   async run() {
     const CAR_NAMES_STR = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const RACE_COUNT = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const CARS = new Cars(CAR_NAMES_STR.split(','));
   }
 }
 
